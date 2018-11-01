b08134b69a0fe6b62b9bbe6362f2129fe37f28cf
Zakir Syed <35612166+zakir8251@users.noreply.github.com>
2018-11-01 13:48:00 -0700

    Create access_xls.py

---
+++ access_xls.py
@@ -0,0 +1,27 @@
+import sys
+import pandas as pd
+
+print('Python version: ' + sys.version)
+print('Pandas version: ' + pd.__version__)
+
+
+df = pd.read_excel('//path/to/file.xlsx', sheet_name=5, usecols=1, header=2, nrows=5) 
+# Load 6th sheet, up to 2nd col and 5 rows, header is row 3; 
+print(df)
+
+# Classic python loop uisng enumerate - Slowest
+for i, row in enumerate(df.values):
+	item = df.index[i]
+	x, y = row
+	print(f"{x} : {y}") 
+print("\n")
+
+# Most used and faster but still slow
+for index, row in df.iterrows():
+	print(f"{(row['Col 1'])}: {(row['Col 2'])}")
+
+print("\n")
+# Fastest
+df.columns = df.columns.str.replace(r'\s+', '_') #This replaces ' ' with '_' in col names
+for row in df.itertuples():
+	print(f"{row.Col_1} : {row.Col_2}")
