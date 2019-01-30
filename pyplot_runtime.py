5d2078664a84d261fcc6b5350cb4c34f81be832c
Zakir Syed <35612166+zakir8251@users.noreply.github.com>
2019-01-30 14:32:26 -0800

    Increased plot size and font size

--- pyplot_runtime.py
+++ pyplot_runtime.py
@@ -2,9 +2,10 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 print('Python version: ' + sys.version)
-print('Numpy version: ' + np.__version__)
+print('Numpy version : ' + np.__version__)
 print('Pandas version: ' + pd.__version__)
 
 x = np.arange(0, 1*np.pi, 0.1)
@@ -12,10 +13,10 @@ y1 = x
 y2 = x*np.log(x)
 y3 = np.log(x)
 
-fig=plt.figure(1)
+fig=plt.figure(1, figsize=(15,7))
 ax = fig.add_subplot(111)
-plt.xlabel("n")
-plt.ylabel(" f(n)")
+plt.xlabel('n', fontsize=18)
+plt.ylabel('f(n)', fontsize=18)
 
 ax.plot(x,y1)
 ax.plot(x,y2)
@@ -27,5 +28,5 @@ ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('zero')
 ax.spines['top'].set_color('none')
 
-plt.legend(['n', 'log(n)','nlog(n)', 'n^2'], loc='upper right')
+plt.legend(['n', 'log(n)','nlog(n)', 'n^2'], loc='upper center', fontsize=18)
 plt.show()
